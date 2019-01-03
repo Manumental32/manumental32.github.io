e3b5abe6d87d6378fb003adb423096c0cbbf69f2
maquino <maquino@arsat.com.ar>
2019-01-03 14:50:23 -0300

    Updates

---
+++ precache-manifest.2de9956d4f7ca950e79a9eaec706395d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactMyApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0bd77ad6c20d8b419be5",
+    "url": "/reactMyApp/static/js/runtime~main.0bd77ad6.js"
+  },
+  {
+    "revision": "bf62dfe2bab03f6f467f",
+    "url": "/reactMyApp/static/js/main.bf62dfe2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/reactMyApp/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "bf62dfe2bab03f6f467f",
+    "url": "/reactMyApp/static/css/main.1ab280b0.chunk.css"
+  },
+  {
+    "revision": "e8abec5d6c5ea0d1cb67f1017172a49c",
+    "url": "/reactMyApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.b1e724021bdee4fc6206deffa8fda981.js"
+  "/reactMyApp/precache-manifest.2de9956d4f7ca950e79a9eaec706395d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myapp/index.html", {
+workbox.routing.registerNavigationRoute("/reactMyApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
