5010de6bc0b1fbdcc8cab4360a38226ad41c4d6d
maquino <maquino@arsat.com.ar>
2019-01-03 11:39:51 -0300

    Updates

---
+++ precache-manifest.b1e724021bdee4fc6206deffa8fda981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "d84ed1796c4ccd74bcd4",
+    "url": "/myapp/static/js/main.d84ed179.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/myapp/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d84ed1796c4ccd74bcd4",
+    "url": "/myapp/static/css/main.1ab280b0.chunk.css"
+  },
+  {
+    "revision": "5d90a2ad78e44e6cc966ac4614b277c8",
+    "url": "/myapp/index.html"
+  }
+];
